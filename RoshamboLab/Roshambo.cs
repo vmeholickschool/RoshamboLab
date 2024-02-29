815044a4cddbe4fa6390f8316fac8404619223c3
Meholick, Vicki <vickimeholick@rocketmortgage.com>
2024-02-28 21:15:53 -0500

    Add project files.

---
+++ RoshamboLab/Roshambo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Create an enumeration called Roshambo that has three values: rock, paper, and scissors.
+
+namespace RoshamboLab
+{
+    public enum Roshambo
+    {
+        rock,
+        paper,
+        scissors
+
+    }
+}
