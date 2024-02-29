401668abd2942ec30e9175462cece61ac999d102
Meholick, Vicki <vickimeholick@rocketmortgage.com>
2024-02-28 21:46:56 -0500

    Working on classes

---
+++ RoshamboLab/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Create an abstract class named Player that stores a name and a Roshambo value.
+//This class should include a method named GenerateRoshambo
+//that allows an inheriting class to generate and return a Roshambo value.
+
+namespace RoshamboLab
+{
+    public abstract class Player
+    {
+        public abstract string Name { get; }
+        public abstract string RoshamboValue { get; }
+
+        public string GenerateRoshambo()
+        {
+            return RoshamboValue;
+        }
+    }
+}
---
+++ RoshamboLab/RockPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoshamboLab
+{
+    internal class RockPlayer : Player
+    {
+        public override string Name { get; } = "Always throws Rock";
+
+        public override string RoshamboValue { get; }
+
+        public RockPlayer(string roshamboValue)
+        {
+            RoshamboValue = roshamboValue;
+        }
+    }
+}
+
